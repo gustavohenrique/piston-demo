fa50db49d0f27d60a4d94e13433ec4aa99e448af
Gustavo Henrique de Oliveira Costa <gustavo.costa@strada.ideais.local>
2011-01-12 19:26:01 -0200

    First commit

---
+++ goal/models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.contrib.auth.models import User
+
+class Goal(models.Model):
+    owner = models.ForeignKey(User)
+    desc = models.CharField(max_length=254)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    achieved = models.BooleanField(default=False)
+    repeat = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.desc
---
+++ goal/tests.py
@@ -0,0 +1,31 @@
+"""
+This file demonstrates two different styles of tests (one doctest and one
+unittest). These will both pass when you run "manage.py test".
+
+Replace these with more appropriate tests for your application.
+"""
+
+from django.test import TestCase
+from goal.models import *
+
+def factory(**kwargs):
+    params = dict(desc='Buy milk', latitude='-19.999208', longitude='-43.967591', achieved=False, repeat=False)
+    params.update(**kwargs)
+    return Goal(**params)
+
+
+class TestModels(TestCase):
+
+    fixtures = ['auth.json', 'goal.json']
+
+    def setUp(self):
+        self.user = User.objects.get(username='admin')
+
+    def test_create_basic_goal(self):
+        goal = factory(owner=self.user)
+        goal.save()
+        self.assertEquals(3, goal.id)
+
+    def test_find_not_achieved_by_user(self):
+        goals = Goal.objects.filter(owner=self.user, achieved=False)
+        self.assertEquals(1, goals.count())
